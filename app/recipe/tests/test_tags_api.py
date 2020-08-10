99265ad7a510e1ff2dad23da306b5dbc6f868f9f
EduBergantini <eduardo_bergantini_pinto>
2020-08-10 02:02:05 -0300

    removed unsued import

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Tag
 from recipe.serializers import TagSerializer
-import json
 
 
 TAGS_URL = reverse("recipe:tag-list")
